2e7f806b968792c92176219c68a7bc01f93d9196
murr123yandex.ru <murr123yandex.ru@outlook.com>
2022-02-10 14:53:54 +0300

    NodeSignalIndicator script added

---
+++ Assets/Scripts/NodeSignalIndicator.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeSignalIndicator : MainBusUser
+{
+    Node _node;
+    SpriteRenderer _indicatorRenderer;
+
+    public string indicatorName = "Indicator";
+    public string nodeBusKey;
+
+    public float periodOfRefreshing;
+
+    public Sprite trueSprite;
+    public Sprite falseSprite;
+
+
+    private void Awake()
+    {
+        ConnectMainBus();
+
+        _indicatorRenderer = transform.Find(indicatorName).GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        _node = mainBus.Get<Node>(nodeBusKey);
+        StartCoroutine(Refresh());
+    }
+
+    void ChangeAtTrue() => _indicatorRenderer.sprite = trueSprite;
+
+    void ChangeAtFalse() => _indicatorRenderer.sprite = falseSprite;
+
+    // TODO: realize on events
+    IEnumerator Refresh()
+    {
+        while(true)
+        {
+            if(_node.Signal)
+                ChangeAtTrue();
+            else
+                ChangeAtFalse();
+
+            yield return new WaitForSeconds(periodOfRefreshing);
+        }
+    }
+}
