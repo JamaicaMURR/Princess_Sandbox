47d1fb75015a4411205544b26c43d955df064108
murr123yandex.ru <murr123yandex.ru@outlook.com>
2022-02-16 00:23:32 +0300

    ExperimentalCaller added for testing

---
+++ Assets/Classes/ExperimentalCaller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Princess
+{
+    public class ExperimentalCaller : ICaller
+    {
+        public (int edgeNumber, float callForce) Straight(float rawDesire, bool[] punchcard, float[] rawcard, int[] solution)
+        {
+            (int edgeNumber, float callForce) callParams = (0, 0);
+
+            float absCallForce = Math.Abs(rawDesire);
+            int bestInSolution = 0;
+
+            for(int i = 0; i < punchcard.Length; i++)
+            {
+                if(punchcard[i])
+                {
+                    if(solution[i] < 0 && rawcard[i] > -absCallForce && -solution[i] > bestInSolution)
+                    {
+                        bestInSolution = -solution[i];
+                        callParams.edgeNumber = i;
+                        callParams.callForce = -absCallForce;
+                    }
+                }
+                else
+                {
+                    if(solution[i] > 0 && rawcard[i] < absCallForce && solution[i] > bestInSolution)
+                    {
+                        bestInSolution = solution[i];
+                        callParams.edgeNumber = i;
+                        callParams.callForce = absCallForce;
+                    }
+                }
+            }
+
+            return callParams;
+        }
+
+        public (int edgeNumber, float callForce) Inverted(float rawDesire, bool[] punchcard, float[] rawcard, int[] solution)
+        {
+            (int edgeNumber, float callForce) callParams = (0, 0);
+
+            // TODO: implementation
+
+            return callParams;
+        }
+    }
+}
